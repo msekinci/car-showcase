a8cc1edf94b59f6b6f1c34a6f4956e90cb5772c5
msekinci <mehmetserkanekinci@gmail.com>
2023-08-16 11:27:56 +0300

    add task with modal

--- apis/api.ts
+++ apis/api.ts
@@ -3,7 +3,22 @@ import { ITask } from "@/types/type";
 const baseUrl = "http://localhost:3001";
 
 export const getAllTodos = async (): Promise<ITask[]> => {
-  const res = await fetch(`${baseUrl}/tasks`);
+  const res = await fetch(`${baseUrl}/tasks`, {cache: 'no-store'});
   const todos = res.json();
   return todos;
 };
+
+export const addTodo = async (todo : ITask): Promise<ITask> => 
+{
+  const res = await fetch(`${baseUrl}/tasks`, {
+    method: 'POST',
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(todo)
+  })
+  
+  const newTodo = await res.json();
+  return newTodo;
+}
+
--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,19 +1,53 @@
-"use client"
+"use client";
 
-import React, { useState } from "react";
+import React, { FormEventHandler, useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import Modal from "./Modal";
+import { addTodo } from "@/apis/api";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
 
 const AddTask = () => {
+  const router = useRouter();
   const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [newTaskValue, setNewTaskValue] = useState<string>("");
+
+  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await addTodo({
+      id: uuidv4(),
+      text: newTaskValue,
+    });
+    setNewTaskValue("");
+    setModalOpen(false);
+    router.refresh();
+  };
 
   return (
     <div>
-      <button onClick={() => setModalOpen(true)} className="btn btn-primary w-full">
+      <button
+        onClick={() => setModalOpen(true)}
+        className="btn btn-primary w-full"
+      >
         Add new task
         <AiOutlinePlus className="ml-2" size={18} />
       </button>
       <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+        <form onSubmit={handleSubmitNewTodo}>
+          <h3 className="font-bold text-lg">Add new task</h3>
+          <div className="modal-action">
+            <input
+              value={newTaskValue}
+              onChange={(e) => setNewTaskValue(e.target.value)}
+              type="text"
+              className="input input-bordered w-full"
+              placeholder="Type here"
+            />
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
       </Modal>
     </div>
   );
